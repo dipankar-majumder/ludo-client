9eb4fd468a246552fc35f6ec529f29f3c9681d0d
Pritam Ghosh <meghoshpritam@gmail.com>
2020-05-13 20:33:56 +0530

    :construction: dice added

---
+++ src/components/Dice.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import style from './Dice.module.scss';
+import RollingDice from './RollingDice';
+
+export default () => {
+  const [diceNumber, setDiceNumber] = useState(0);
+  const [rolling, setRolling] = useState(false);
+
+  const clickHandler = () => {
+    setRolling(true);
+    setTimeout(() => {
+      setRolling(false);
+      setDiceNumber(Math.floor(Math.random() * 6) + 1);
+    }, 1000);
+  };
+
+  return (
+    <div onClick={clickHandler}>
+      {rolling && <RollingDice />}
+      {!rolling && (
+        <div className={style.container}>
+          <div className={style.diceWrapper}>
+            <div className={style[`dice${diceNumber}`]}>
+              {Array.from(Array(diceNumber).keys()).map(el => (
+                <div key={el} />
+              ))}
+              {diceNumber === 0 && <div>Roll</div>}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
---
+++ src/components/RollingDice.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+import style from './RollingDice.module.scss';
+
+export default () => {
+  return (
+    <div className={style.main}>
+      <div className={style.diceContainer}>
+        <div className={style.dice}>
+          <div className={style.front}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={style.back}>
+            <span></span>
+          </div>
+          <div className={style.right}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={style.left}>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={style.top}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={style.bottom}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
